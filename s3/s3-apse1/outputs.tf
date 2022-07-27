8feb333c8eab0500c3a35ee546fc99da4348ad1c
Visali0412 <95960688+Visali0412@users.noreply.github.com>
2022-07-27 23:09:48 +0530

    Delete s3/s3-apse1 directory

--- s3/s3-apse1/outputs.tf
+++
@@ -1,8 +0,0 @@
-# Set outputs
-output "your_domain_bucket_website_endpoint" {
-  value = module.your_domain.bucket_website_endpoint
-}
-
-output "your_domain_bucket_hosted_zone_id" {
-  value = module.your_domain.bucket_hosted_zone_id
-}
