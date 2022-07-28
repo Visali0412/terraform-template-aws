0f633720a6b52409f77fbb209ea6758c2271e8a2
Visali0412 <95960688+Visali0412@users.noreply.github.com>
2022-07-28 12:58:38 +0530

    Update ecr.tf

--- ecr/ecr-apse1/ecr.tf
+++ ecr/ecr-apse1/ecr.tf
@@ -1,6 +1,6 @@
 # Create ECR Repository
 resource "aws_ecr_repository" "demo_repo" {
-  name                 = "your-repository-name"
+  name                 = var.repo_name
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
@@ -8,7 +8,7 @@ resource "aws_ecr_repository" "demo_repo" {
   }
 }
 
-resource "aws_ecr_lifecycle_policy" "demo_repo" {
+resource "aws_ecr_lifecycle_policy" "display_last10_images" {
   repository = aws_ecr_repository.demo_repo.name
 
   policy = file("./json-policies/expire-untagged.json")
